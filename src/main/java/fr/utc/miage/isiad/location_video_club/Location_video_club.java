2c56134e09533e651edd96e3e8b6768ef3ec5aa6
madiassalakamyd <madiassalakamyd@gmail.com>
2024-11-04 15:32:49 +0100

    first commit

---
+++ src/main/java/fr/utc/miage/isiad/location_video_club/Location_video_club.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.utc.miage.isiad.location_video_club;
+
+/**
+ *
+ * @author SaidTec
+ */
+public class Location_video_club {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
